65ab0fc46e97f732eda1c0740f63d5588c68442f
SamraAzizi <fahimaazizi369@gmail.com>
2024-06-05 11:57:25 +0500

    my commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame 
+
+pygame.init()
+WIDTH = 1000
+HEIGHT = 900
+
+screen = pygame.display.set_mode(WIDTH, HEIGHT)
+font = pygame.font.Font('freesansbold.ttf', 20)
+big_font = pygame.font.Font('freesandbold', 50)
+timer = pygame.time.clock()
+fps = 60
+
+
+#game variable and images
+
