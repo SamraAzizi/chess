8d5212fb2f1de961de56bc1a6a83a64e40f09fbd
SamraAzizi <fahimaazizi369@gmail.com>
2024-06-05 12:06:28 +0500

    Your commit message

--- main.py
+++ main.py
@@ -4,12 +4,31 @@ pygame.init()
 WIDTH = 1000
 HEIGHT = 900
 
-screen = pygame.display.set_mode(WIDTH, HEIGHT)
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
 font = pygame.font.Font('freesansbold.ttf', 20)
-big_font = pygame.font.Font('freesandbold', 50)
-timer = pygame.time.clock()
+big_font = pygame.font.Font('freesansbold.ttf', 50)
+timer = pygame.time.Clock()
 fps = 60
 
 
 #game variable and images
 
+
+#main game loop
+
+run = True
+while run:
+    timer.tick(fps)
+    screen.fill('dark gray')
+    
+    #event handling 
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    
+    pygame.display.flip()
+
+
+pygame.quit()
