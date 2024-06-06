105673bfb71d865f8a4a57b3ca4826dace2852d7
SamraAzizi <fahimaazizi369@gmail.com>
2024-06-06 13:44:02 +0500

    Your commit message

--- main.py
+++ main.py
@@ -119,7 +119,13 @@ white_pawn = pygame.image.load("assets/images/white pawn.png")
 white_pawn = pygame.transform.scale(white_pawn,(65,65))
 white_pawn_small = pygame.transform.scale(white_pawn,(45,45))
 
+white_images = [white_pawn, white_queen, white_king, white_knight, white_rook, white_bishop ]
+small_white_images = [white_pawn_small, white_queen_small, white_king_small, white_knight_small, white_rook_small, white_bishop_small ]
 
+
+
+black_images = [black_pawn, black_queen, black_king, black_knight, black_rook, black_bishop ]
+small_black_images = [black_pawn_small, black_queen_small, black_king_small, black_knight_small, black_rook_small, black_bishop_small ]
 #main game loop
 
 run = True
