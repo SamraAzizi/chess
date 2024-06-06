9e95a1c9c8acc2ede2bed7775e515d9436edaae8
SamraAzizi <fahimaazizi369@gmail.com>
2024-06-06 13:46:14 +0500

    Your commit message

--- main.py
+++ main.py
@@ -126,6 +126,10 @@ small_white_images = [white_pawn_small, white_queen_small, white_king_small, whi
 
 black_images = [black_pawn, black_queen, black_king, black_knight, black_rook, black_bishop ]
 small_black_images = [black_pawn_small, black_queen_small, black_king_small, black_knight_small, black_rook_small, black_bishop_small ]
+
+
+
+piece_list = ['pawn','queen','king','bishop','rook','knight']
 #main game loop
 
 run = True
