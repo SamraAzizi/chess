50d08fb640c7ebfe506079161900a842418da3df
SamraAzizi <fahimaazizi369@gmail.com>
2024-06-13 11:48:57 +0500

    Your commit message

--- main.py
+++ main.py
@@ -453,7 +453,13 @@ def draw_captured():
     for i in range(len(captured_pieces_white)):
         captured_piece = captured_pieces_white[i]
         index = piece_list.index(captured_piece)
-        screen.blit(small_black_images)
+        screen.blit(small_black_images[index] , (825, 5 + 50*i))
+
+
+    for i in range(len(captured_pieces_black)):
+        captured_piece = captured_pieces_black[i]
+        index = piece_list.index(captured_piece)
+        screen.blit(small_white_images[index] , (825, 5 + 50*i))
 
 
 #main game loop
