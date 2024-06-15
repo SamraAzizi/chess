b67ca105f1a218e75240f3fbab344072e917aea3
SamraAzizi <fahimaazizi369@gmail.com>
2024-06-15 11:55:46 +0500

    Your commit message

--- main.py
+++ main.py
@@ -17,15 +17,8 @@ fps = 60
 white_pieces = ['knight','bishop','king','queen','bishop','knight','rook'
                 ,'pawn' ,'pawn' ,'pawn' ,'pawn' ,'pawn' ,'pawn' ,'pawn' ,'pawn' ]
 
-
-
 white_location = [(0,0),(1.0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0)
                   ,(0,1),(1.1),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1)]
-
-
-
-
-
 
 black_pieces = ['knight','bishop','king','queen','bishop','knight','rook'
                 ,'pawn' ,'pawn' ,'pawn' ,'pawn' ,'pawn' ,'pawn' ,'pawn' ,'pawn' ]
@@ -585,7 +578,25 @@ while run:
                 selection = 100
                 valid_moves = []
         if event.type == pygame.KEYDOWN and game_over:
-            if event.key == pygame.K_RETURN     
+            if event.key == pygame.K_RETURN:
+                game_over = False
+                winner = ''     
+
+
+                white_pieces = ['knight','bishop','king','queen','bishop','knight','rook'
+                ,'pawn' ,'pawn' ,'pawn' ,'pawn' ,'pawn' ,'pawn' ,'pawn' ,'pawn' ]
+
+                white_location = [(0,0),(1.0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0)
+                  ,(0,1),(1.1),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1)]
+
+                black_pieces = ['knight','bishop','king','queen','bishop','knight','rook'
+                ,'pawn' ,'pawn' ,'pawn' ,'pawn' ,'pawn' ,'pawn' ,'pawn' ,'pawn' ]
+
+                black_location = [(0,7),(1,7),(2,7),(3,7),(4,7),(5,7),(6,7),(7,7)
+                  ,(0,6),(1,6),(2,6),(3,6),(4,6),(5,6),(6,6),(7,6)]
+
+                captured_pieces_white = []
+                captured_pieces_black = []
 if winner != '':
     game_over = True
     draw_game_over()
